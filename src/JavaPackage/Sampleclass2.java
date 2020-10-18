c7cd90db7fb1bb8dc0bb45ca9dab002d21b1dfea
siva6470 <kammasiva9@gmal.com>
2020-10-18 20:08:34 +0530

    commiting second class

---
+++ src/JavaPackage/Sampleclass2.java
@@ -0,0 +1,12 @@
+package JavaPackage;
+
+public class Sampleclass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("printing second class");
+
+	}
+
+}
